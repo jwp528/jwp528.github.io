fcfcba1d099acca6bbf2a6b85cf252e171100393
jwp528 <joshwp528@gmail.com>
2014-10-25 23:19:18 -0400

    responsive css

--- g_bingo.js
+++ g_bingo.js
@@ -84,7 +84,7 @@ function evaluateRow() {
   if ($("#g1").hasClass("selected") && $("#a1").hasClass("selected") && $("#r1").hasClass("selected") && $("#t1").hasClass("selected") && $("#h1").hasClass("selected")){
     document.getElementById("win").play();
     alert("g1, a1, r1, t1, h1 - That's a line!\n\tGARTH!");
-    sleep(3000);
+    sleep(1000);
     clear_board();
     bingo_start();
   }//end if
@@ -92,7 +92,7 @@ function evaluateRow() {
   if ($("#g2").hasClass("selected") && $("#a2").hasClass("selected") && $("#r2").hasClass("selected") && $("#t2").hasClass("selected") && $("#h2").hasClass("selected")){
     document.getElementById("win").play();
     alert("g2, a2, r2, t2, h2 - That's a line!\n\tGARTH!");
-    sleep(3000);
+    sleep(1000);
     clear_board();
     bingo_start();
   }
@@ -100,7 +100,7 @@ function evaluateRow() {
   if ($("#g3").hasClass("selected") && $("#a3").hasClass("selected") && $("#r3").hasClass("selected") && $("#t3").hasClass("selected") && $("#h3").hasClass("selected")){
     document.getElementById("win").play();
     alert("g3, a3, r3, t3, h3 - That's a line!\n\tGARTH!");
-    sleep(3000);
+    sleep(1000);
     clear_board();
     bingo_start();
   }
@@ -108,7 +108,7 @@ function evaluateRow() {
   if ($("#g4").hasClass("selected") && $("#a4").hasClass("selected") && $("#r4").hasClass("selected") && $("#t4").hasClass("selected") && $("#h4").hasClass("selected")){
     document.getElementById("win").play();
     alert("g4, a4, r4, t4, h4 - That's a line!\n\tGARTH!");
-    sleep(3000);
+    sleep(1000);
     clear_board();
     bingo_start();
   }
@@ -116,7 +116,7 @@ function evaluateRow() {
   if ($("#g5").hasClass("selected") && $("#a5").hasClass("selected") && $("#r5").hasClass("selected") && $("#t5").hasClass("selected") && $("#h5").hasClass("selected")){
     document.getElementById("win").play();
     alert("g5, a5, r5, t5, h5 - That's a line!\n\tGARTH!");
-    sleep(3000);
+    sleep(1000);
     clear_board();
     bingo_start();
   }
@@ -126,7 +126,7 @@ function evaluateCol() {
   if ($("#g1").hasClass("selected") && $("#g2").hasClass("selected") && $("#g3").hasClass("selected") && $("#g4").hasClass("selected") && $("#g5").hasClass("selected")){
     document.getElementById("win").play();
     alert("g1, g2, g3, g4, g5 - That's a column!\n\tGARTH!");
-    sleep(3000);
+    sleep(1000);
     clear_board();
     bingo_start();
   }//end if
@@ -134,7 +134,7 @@ function evaluateCol() {
   if ($("#a1").hasClass("selected") && $("#a2").hasClass("selected") && $("#a3").hasClass("selected") && $("#a4").hasClass("selected") && $("#a5").hasClass("selected")){
     document.getElementById("win").play();
     alert("a1, a2, a3, a4, a5 - That's a column!\n\tGARTH!");
-    sleep(3000);
+    sleep(1000);
     clear_board();
     bingo_start();
   }//end if
@@ -142,7 +142,7 @@ function evaluateCol() {
   if ($("#r1").hasClass("selected") && $("#r2").hasClass("selected") && $("#r3").hasClass("selected") && $("#r4").hasClass("selected") && $("#r5").hasClass("selected")){
     document.getElementById("win").play();
     alert("r1, r2, r3, r4, r5 - That's a column!\n\tGARTH!");
-    sleep(3000);
+    sleep(1000);
     clear_board();
     bingo_start();
   }//end if
@@ -150,7 +150,7 @@ function evaluateCol() {
   if ($("#t1").hasClass("selected") && $("#t2").hasClass("selected") && $("#t3").hasClass("selected") && $("#t4").hasClass("selected") && $("#t5").hasClass("selected")){
     document.getElementById("win").play();
     alert("t1, t2, t3, t4, t5 - That's a column!\n\tGARTH!");
-    sleep(3000);
+    sleep(1000);
     clear_board();
     bingo_start();
   }//end if
@@ -158,7 +158,7 @@ function evaluateCol() {
   if ($("#h1").hasClass("selected") && $("#h2").hasClass("selected") && $("#h3").hasClass("selected") && $("#h4").hasClass("selected") && $("#h5").hasClass("selected")){
     document.getElementById("win").play();
     alert("h1, h2, h3, h4, h5 - That's a column!\n\tGARTH!");
-    sleep(3000);
+    sleep(1000);
     clear_board();
     bingo_start();
   }//end if
@@ -168,7 +168,7 @@ function evaluateDiagonal(){
   if ($("#g1").hasClass("selected") && $("#a2").hasClass("selected") && $("#r3").hasClass("selected") && $("#t4").hasClass("selected") && $("#h5").hasClass("selected")){
     document.getElementById("win").play();
     alert("g1, a2, r3, t4, h5 - That's a cross!\n\tGARTH!");
-    sleep(3000);
+    sleep(1000);
     clear_board();
     bingo_start();
   }//end if
@@ -176,7 +176,7 @@ function evaluateDiagonal(){
   if ($("#g5").hasClass("selected") && $("#a4").hasClass("selected") && $("#r3").hasClass("selected") && $("#t2").hasClass("selected") && $("#h1").hasClass("selected")){
     document.getElementById("win").play();
     alert("g5, a4, r3, t2, h1 - That's a cross!\n\tGARTH!");
-    sleep(3000);
+    sleep(1000);
     clear_board();
     bingo_start();
   }//end if
