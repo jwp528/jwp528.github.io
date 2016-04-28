edc72faf5c5924faf7372002690237fb7fe8b81f
Josh Parsons <joshwp528@gmail.com>
2016-04-28 02:03:04 -0400

    footer updated for social media

---
+++ include/footer.js
@@ -0,0 +1,8 @@
+/**
+ *Header js
+ *
+ *contains the footer for each page of the website
+ */
+var foot = "<div class='social'><a href='http://www.facebook.com/jwp528' title='Find me on Facebook'><div class='media facebook'></div></a><a href='https://twitter.com/josh_parsons528' title='tweet me @josh_parsons528'><div class='media twitter'></div></a><a href='http://ca.linkedin.com/in/jwp528/en' title='Check out my LinkedIn Profile'><div class='media lin'></div></a></div>";
+$(".footer").append(foot);
+
